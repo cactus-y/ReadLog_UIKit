199c0696c5049f4e152bfc07209df662fc1aea25
Seokwon You <youseokwon1667@gmail.com>
2024-01-25 21:37:12 +0900

    Project Setting

---
+++ ReadLog_UIKit/ReadLog_UIKit/Presentation/ViewController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadLog_UIKit
+//
+//  Created by 유석원 on 1/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
